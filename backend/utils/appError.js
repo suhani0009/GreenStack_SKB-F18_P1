5bfe606b93ac7af050c51b5c51a2e69c8e907706
suhani09 <suhanirangari090105@gmail.com>
2026-04-18 10:16:13 +0530

    Delete backend directory

--- backend/utils/appError.js
+++
@@ -1,9 +0,0 @@
-class AppError extends Error {
-  constructor(message, statusCode = 500, details = null) {
-    super(message);
-    this.statusCode = statusCode;
-    this.details = details;
-  }
-}
-
-module.exports = AppError;
--- backend/utils/catchAsync.js
+++
@@ -1,5 +0,0 @@
-const catchAsync = (handler) => (req, res, next) => {
-  Promise.resolve(handler(req, res, next)).catch(next);
-};
-
-module.exports = catchAsync;
